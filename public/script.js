39e743c2076bc88ea29172979be6649ffe55461e
ImpostorEngineer <posta.gereksiz@gmail.com>
2021-02-07 13:55:04 -0500

    Add card overlay

---
+++ public/script.js
@@ -0,0 +1,9 @@
+let overlays = document.querySelector('#test');
+console.log(overlays);
+// function display() {
+//   for (let i = 0; i < overlays.length; i++) {
+//     overlays[i].classList.remove('hidden');
+//   }
+// }
+
+// display();
